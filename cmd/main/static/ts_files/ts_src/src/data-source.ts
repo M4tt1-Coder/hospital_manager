cb9a027d9fd2fb2fbcfcbed30e0ecb75ef946c3b
M4tt1-Coder <geimat75@gmail.com>
2023-06-15 18:52:58 +0200

    porgress in database + new ways to solve it

--- cmd/main/static/ts_files/ts_src/src/data-source.ts
+++ cmd/main/static/ts_files/ts_src/src/data-source.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
+import { DataSource, ConnectionOptions } from "typeorm"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
